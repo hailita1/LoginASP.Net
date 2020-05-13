38a356c673840df3ad0d95939a22b420d7a6b2a7
Hai Lit <hailit2306@gmail.com>
2020-05-13 18:14:22 +0700

    first commit

---
+++ Login/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserDao userDao = new UserDao();
+                var result = userDao.Login(model.Username, model.Password);
+                if (result)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập thất bại");
+                }
+            }
+            return View();
+        }
+    }
+}
